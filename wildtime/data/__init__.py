0125e0f766be71b8ac7f21be3fd5817129a796c1
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-10-24 17:56:21 -0400

    Update __init__.py

--- wildtime/data/__init__.py
+++ wildtime/data/__init__.py
@@ -1,5 +1,5 @@
 from .arxiv import ArXiv, ArXivGroup
-from .drug import TdcDtiDg, TdcDtiDgGroup
-from .fmow import FMoW, FMoWGroup
-from .huffpost import HuffPost, HuffPostGroup
+#from .drug import TdcDtiDg, TdcDtiDgGroup
+#from .fmow import FMoW, FMoWGroup
+#from .huffpost import HuffPost, HuffPostGroup
 from .yearbook import Yearbook, YearbookGroup
