e4c1350f3f2a3317a9d0d4ffa1ac5b5b33251829
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-10-30 06:11:02 -0400

    Update main.py

--- wildtime/main.py
+++ wildtime/main.py
@@ -7,6 +7,7 @@ import torch
 from torch import cuda
 
 from baseline_trainer import trainer_init
+from config import config
 from wildtime.methods.agem.agem import AGEM
 from wildtime.methods.coral.coral import DeepCORAL
 from wildtime.methods.erm.erm import ERM
@@ -23,23 +24,27 @@ device = 'cuda' if cuda.is_available() else 'cpu'
 
 
 if __name__ == '__main__':
+    # Load defaults from config file
+    default_configs = argparse.Namespace(**config)
+
+    # Set up argument parser
     parser = argparse.ArgumentParser(description="Run WildTime training")
     
-    # Define the arguments
-    parser.add_argument('--dataset', type=str, required=True, help='Dataset name')
-    parser.add_argument('--method', type=str, required=True, choices=['groupdro', 'coral', 'irm', 'ft', 'erm', 'ewc', 'agem', 'si', 'simclr', 'swav', 'swa'], help='Method to use')
-    parser.add_argument('--offline', action='store_true', help='Run in offline mode')
-    parser.add_argument('--mini_batch_size', type=int, default=32, help='Mini batch size')
-    parser.add_argument('--train_update_iter', type=int, default=6000, help='Number of training update iterations')
-    parser.add_argument('--lr', type=float, default=2e-5, help='Learning rate')
-    parser.add_argument('--weight_decay', type=float, default=0.01, help='Weight decay')
-    parser.add_argument('--split_time', type=int, default=1970, help='Split time for dataset')
-    parser.add_argument('--num_workers', type=int, default=8, help='Number of workers for data loading')
-    parser.add_argument('--random_seed', type=int, default=1, help='Random seed')
-    parser.add_argument('--log_dir', type=str, required=True, help='Directory for saving logs and models')
-    parser.add_argument('--data_dir', type=str, default='./data', help='Data directory')
-    parser.add_argument('--results_dir', type=str, default='./results', help='Results directory')
-    parser.add_argument('--load_model', action='store_true', help='Load model from checkpoint')
+    # Add arguments with defaults from config
+    parser.add_argument('--dataset', type=str, default=default_configs.dataset, help='Dataset name')
+    parser.add_argument('--method', type=str, default=default_configs.method, choices=['groupdro', 'coral', 'irm', 'ft', 'erm', 'ewc', 'agem', 'si', 'simclr', 'swav', 'swa'], help='Method to use')
+    parser.add_argument('--offline', action='store_true', default=default_configs.offline, help='Run in offline mode')
+    parser.add_argument('--mini_batch_size', type=int, default=default_configs.mini_batch_size, help='Mini batch size')
+    parser.add_argument('--train_update_iter', type=int, default=default_configs.train_update_iter, help='Number of training update iterations')
+    parser.add_argument('--lr', type=float, default=default_configs.lr, help='Learning rate')
+    parser.add_argument('--weight_decay', type=float, default=default_configs.weight_decay, help='Weight decay')
+    parser.add_argument('--split_time', type=int, default=default_configs.split_time, help='Split time for dataset')
+    parser.add_argument('--num_workers', type=int, default=default_configs.num_workers, help='Number of workers for data loading')
+    parser.add_argument('--random_seed', type=int, default=default_configs.random_seed, help='Random seed')
+    parser.add_argument('--log_dir', type=str, default=default_configs.log_dir, help='Directory for saving logs and models')
+    parser.add_argument('--data_dir', type=str, default=default_configs.data_dir, help='Data directory')
+    parser.add_argument('--results_dir', type=str, default=default_configs.results_dir, help='Results directory')
+    parser.add_argument('--load_model', action='store_true', default=default_configs.load_model, help='Load model from checkpoint')
 
     # Parse arguments
     configs = parser.parse_args()
