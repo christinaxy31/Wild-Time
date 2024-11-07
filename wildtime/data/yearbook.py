031e7d3a7b482a8f489ab5d10a74319ada58481c
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-11-07 15:14:42 -0500

    Update yearbook.py

--- wildtime/data/yearbook.py
+++ wildtime/data/yearbook.py
@@ -113,11 +113,14 @@ def preprocess_orig(args):
         dataset[year][2]['images'] = np.stack(images[year], axis=0) / 255.0
         dataset[year][2]['labels'] = np.array(labels[year])
         dataset[year][3] = {}
-        dataset[year][3]['images'] = np.stack(images[year], axis=0) / 255.0
-        dataset[year][3]['labels'] = np.array(labels[year])
+        dataset[year][3]['images'] = np.stack(train_images[year], axis=0) / 255.0
+        dataset[year][3]['labels'] = np.array(train_labels[year])
         dataset[year][4] = {}
-        dataset[year][4]['images'] = np.stack(images[year], axis=0) / 255.0
-        dataset[year][4]['labels'] = np.array(labels[year])
+        dataset[year][4]['images'] = np.stack(test_images[year], axis=0) / 255.0
+        dataset[year][4]['labels'] = np.array(test_labels[year])
+        dataset[year][5] = {}
+        dataset[year][5]['images'] = np.stack(images[year], axis=0) / 255.0
+        dataset[year][5]['labels'] = np.array(labels[year])
 
     preprocessed_data_path = os.path.join(args.data_dir, 'yearbook.pkl')
     pickle.dump(dataset, open(preprocessed_data_path, 'wb'))
