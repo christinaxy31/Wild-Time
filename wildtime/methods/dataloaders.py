da49a934c33a3ba8630d3e7d06cf68783e6e6c00
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-11-07 20:09:19 -0500

    Update dataloaders.py

--- wildtime/methods/dataloaders.py
+++ wildtime/methods/dataloaders.py
@@ -1,6 +1,24 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler, RandomSampler, BatchSampler
+import numpy as np
 
+def proportional_collate_fn(batch, dataset, proportion):
+    
+    year_data = dataset.datasets[dataset.current_time][dataset.mode]
+    images = year_data['images']
+    labels = year_data['labels']
+    
+    num_samples = len(images)
+    sample_size = int(num_samples * proportion)
+    
+    sampled_indices = np.random.choice(num_samples, sample_size, replace=False)
+    
+    sampled_images = images[sampled_indices]
+    sampled_labels = labels[sampled_indices]
+    image_tensors = torch.FloatTensor(sampled_images).permute(0, 3, 1, 2)
+    label_tensors = torch.LongTensor(sampled_labels)
+    
+    return image_tensors, label_tensors
 
 class _InfiniteSampler(torch.utils.data.Sampler):
     """Wraps another Sampler to yield an infinite stream."""
@@ -36,6 +54,39 @@ class InfiniteDataLoader:
             num_workers=num_workers,
             batch_sampler=_InfiniteSampler(batch_sampler),
             collate_fn=collate_fn
+        ))
+
+    def __iter__(self):
+        while True:
+            yield next(self._infinite_iterator)
+
+    def __len__(self):
+        raise ValueError
+
+
+
+class ProportionalDataLoader:
+    def __init__(self, dataset, weights, batch_size, num_workers, collate_fn=None):
+        super().__init__()
+
+        if weights is not None:
+            sampler = torch.utils.data.WeightedRandomSampler(weights,
+                                                             replacement=True,
+                                                             num_samples=batch_size)
+        else:
+            sampler = torch.utils.data.RandomSampler(dataset,
+                                                     replacement=True)
+
+        batch_sampler = torch.utils.data.BatchSampler(
+            sampler,
+            batch_size=min(batch_size, len(dataset)),
+            drop_last=True)
+
+        self._infinite_iterator = iter(DataLoader(
+            dataset,
+            num_workers=num_workers,
+            batch_sampler=_InfiniteSampler(batch_sampler),
+            collate_fn=lambda batch: proportional_collate_fn(batch, dataset, proportion)
         ))
 
     def __iter__(self):
