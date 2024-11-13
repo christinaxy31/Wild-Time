818a84fcb6bceb0a8faf503f76eba9dc389a0686
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-11-13 07:17:11 -0500

    Update base_trainer.py

--- wildtime/methods/base_trainer.py
+++ wildtime/methods/base_trainer.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from sklearn import metrics
 from tdc import Evaluator
 
-from .dataloaders import FastDataLoader, InfiniteDataLoader, ProportionalDataLoader
+from .dataloaders import FastDataLoader, InfiniteDataLoader, ProportionalDataLoader, CombinedInfiniteDataLoader
 from .utils import prepare_data, forward_pass, get_collate_functions
 
 
