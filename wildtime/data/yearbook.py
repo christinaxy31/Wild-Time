c7d6bee11c9b963dd1a670e01141a72869b87330
christinaxy31 <145334096+christinaxy31@users.noreply.github.com>
2024-11-19 10:43:17 -0500

    Update yearbook.py

--- wildtime/data/yearbook.py
+++ wildtime/data/yearbook.py
@@ -259,11 +259,11 @@ class YearbookBase(Dataset):
     def update_historical(self, idx, data_del=False):
         time = self.ENV[idx]
         prev_time = self.ENV[idx - 1]
-        print(time)
-        print(prev_time)
+        #print(time)
+        #print(prev_time)
         print(self.mode)
-        print(self.datasets.keys())
-        print(self.datasets[prev_time].keys())
+        #print(self.datasets.keys())
+        #print(self.datasets[prev_time].keys())
         self.datasets[time][self.mode]['images'] = np.concatenate(
             (self.datasets[time][self.mode]['images'], self.datasets[prev_time][self.mode]['images']), axis=0)
         self.datasets[time][self.mode]['labels'] = np.concatenate(
